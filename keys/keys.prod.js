ea569f6c87b27efd1b733d64f8ef40189e1f9a1b
Ilia Kozionov <68503974+Pardjai@users.noreply.github.com>
2022-04-23 14:42:10 +0300

    Add files via upload

---
+++ keys/keys.prod.js
@@ -0,0 +1,11 @@
+module.exports = {
+    MONGODB_URI: process.env.MONGODB_URI,
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_CLIENT_ID: process.env.EMAIL_CLIENT_ID,
+    EMAIL_CLIENT_SECRET: process.env.EMAIL_CLIENT_SECRET,
+    EMAIL_REFRESH_TOKEN: process.env.EMAIL_REFRESH_TOKEN,
+    BASE_URL: process.env.BASE_URL,
+ }
